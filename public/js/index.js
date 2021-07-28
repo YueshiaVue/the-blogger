2823049a1e9529a89d8a92c0444c7e0d972dfffb
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-07-28 18:43:53 -0500

    added post script to fetch adda

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,13 @@
 let postButton = document.getElementById("post-button");
 let postTitleInputEl = document.getElementById("post-title-input");
-let postCommentInputEl = document.getElementById("post-comment-input");
+let postBodyInputEl = document.getElementById("post-body-input");
 let postTitle = postTitleInputEl.value;
-let postComment = postCommentInputEl.value;
+let postBody = postBodyInputEl.value;
 
 postButton.onClick = async ()=> {
     fetch('/api/post'{
         method: 'POST',
-        body: JSON.stringify({title:postTitle,comment:postComment}),
+        body: JSON.stringify({title:postTitle,body:postBody}),
         headers: {
           'Content-Type': 'application/json',
         },
